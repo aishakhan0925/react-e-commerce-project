fc28f47772fb00fbd64c0b7ff08cfba1885c25c7
Aisha Khan <ak3313069@gmail.com>
2020-03-18 00:43:27 +0500

    final E-store

--- src/components/details.js
+++ src/components/details.js
@@ -37,7 +37,7 @@ export default class Details extends Component {
                 {/* product text */}
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
                   <h2> Model : {title} </h2>
-                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                  <h4 className=" text-uppercase text-muted mt-3 mb-2">
                     Made by : {company}
                   </h4>
                   <h4 className="text-blue">
